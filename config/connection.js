eb03bc70e2b6253819e75033f6471ec6a20bd8f0
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2020-12-15 15:08:33 -0600

    application working locally, need to clean code and push to Heroku

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Dependencies
+const mysql = require("mysql");
+
+// Define connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "mysql1234",
+    database: "burgers_db"
+});
+
+// Connect to the server
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connect as ID: " + connection.threadId + "\n");
+});
+
+module.exports = connection;
